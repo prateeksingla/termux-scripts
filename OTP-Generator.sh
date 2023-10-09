86c4d18911f56b48e7a3f6e1d3f4e8788a436dfd
Prateek Singla <singla.prateek@gmail.com>
2023-10-09 17:16:01 +0530

    Update OTP-Generator.sh
    Errors fixed

--- OTP-Generator.sh
+++ OTP-Generator.sh
@@ -1,6 +1,13 @@
 fingerprint_output=$(termux-fingerprint)
+# Example output is shown below:
+# {
+# "errors": [],
+# "failed_attempts": 0,
+# "auth_result": "AUTH_RESULT_SUCCESS"
+# }
+
 auth_result=$(echo "$fingerprint_output" | grep 'AUTH_RESULT')
-if [ "$auth_result" = *"AUTH_RESULT_SUCCESS"* ]; then
+if [[ "$auth_result" == *"AUTH_RESULT_SUCCESS"* ]]; then
     termux-clipboard-set $(oathtool -b --totp 'YOUR_SECRET_KEY')
     termux-toast $(oathtool -b --totp 'YOUR_SECRET_KEY')
 else
