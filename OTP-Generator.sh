5c2d3f53109c444365eafcaa623d08b3bcd3c2dc
Prateek Singla <singla.prateek@gmail.com>
2023-10-09 16:46:42 +0530

    Create OTP-Generator.sh
    Time based OTP generator in shell using Termux script.

---
+++ OTP-Generator.sh
@@ -0,0 +1,9 @@
+fingerprint_output=$(termux-fingerprint)
+auth_result=$(echo "$fingerprint_output" | grep 'AUTH_RESULT')
+if [ "$auth_result" = *"AUTH_RESULT_SUCCESS"* ]; then
+    termux-clipboard-set $(oathtool -b --totp 'YOUR_SECRET_KEY')
+    termux-toast $(oathtool -b --totp 'YOUR_SECRET_KEY')
+else
+    echo "Authentication failed. Script execution terminated."
+    termux-toast "Execution of script failed."
+fi
