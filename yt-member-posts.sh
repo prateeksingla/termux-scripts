495984ba6f2d393b24fce541706a34ae2e3163e8
Prateek Singla <singla.prateek@gmail.com>
2023-11-21 17:19:24 +0530

    Update yt-member-posts.sh

--- yt-member-posts.sh
+++ yt-member-posts.sh
@@ -1,7 +1,15 @@
-wget --load-cookies cookies.txt -p -k https://www.youtube.com/@AkshatZayn/membership
-grep 'contentText' www.youtube.com/@AkshatZayn/membership > extracted_lines
+wget --load-cookies cookies.txt -k -O membership https://www.youtube.com/@AkshatZayn/membership
+grep 'contentText' membership > extracted_lines
 grep -oP 'contentText\K.*?(?=expandButton)' extracted_lines > final.txt
 cut -c 21- final.txt > Member_posts.txt
 grep -Fvf <(cut -c 1-20 posts.txt) Member_posts.txt | cat - posts.txt > temp && mv temp posts.txt
 rm final.txt extracted_lines Member_posts.txt
-rm -rf www.youtube.com
+
+jq --raw-input --slurp '{"description":"An updated gist description","files":{"akshat.txt":{"content": . }}}' posts.txt \
+ | curl -L \
+  -X PATCH \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: [token]" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  https://api.github.com/gists/GIST-ID \
+  -d @-
